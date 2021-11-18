0d803c204e973906db1d5d7c6d40f9076a627051
dominik-schumbert <64913353+dominik-schumbert@users.noreply.github.com>
2021-11-18 14:52:07 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import FirstComponent from './components/FirstComponent'
 import UserComponent from './components/UserComponent'
+import ExampleComponent from './components/ExampleComponent'
 ReactDOM.render(
     <div>
       <h1>Hello, Welcome to React and TypeScript</h1>
       <FirstComponent/>
       <UserComponent name="John Doe" age={26} address="87 Summer St, Boston, MA 02110" dob={new Date()} />
+      <ExampleComponent/>
     </div>,
     document.getElementById("root")
 );
